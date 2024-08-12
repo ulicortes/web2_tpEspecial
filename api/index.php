51d19aff34ca1657c9216ccddaca0fae2ae32e50
ulicortes <74629456+ulicortes@users.noreply.github.com>
2024-08-12 16:35:47 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once '../Controllers/HomeController.php';
+    require_once './Controllers/HomeController.php';
 // api/index.php
     $homeController = new HomeController;
     $homeController->showhome();
